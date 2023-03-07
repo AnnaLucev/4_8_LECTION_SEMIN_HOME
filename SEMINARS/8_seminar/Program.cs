2d61f3d1ee29527d7001878cc5ef3908332ef5c0
Anna Lutsevich <alucevich@mail.ru>
2023-03-07 22:08:22 +0700

    Add lessons and homework seminar 8

---
+++ SEMINARS/8_seminar/Program.cs
@@ -0,0 +1,220 @@
+﻿Console.Clear();
+Console.WriteLine("1 - Программа для замены местами первой и последней строк двумерного массива (задача 53)");
+Console.WriteLine("2 - Программа для транспонирования массива (перемена строк столбцами) (задача 55)");
+Console.WriteLine("3 - Программа для составления частотного словаря элементов двумерного массива (задача 57)");
+Console.WriteLine("4 - Программа, удаляющая строку и столбец, на пересечении которых расположен наименьший элемент массива (задача 59)");
+
+int numberOfTask = Prompt("Введите номер задачи ");
+switch (numberOfTask)
+{
+    case 1:
+    ChangeRowsArray();
+    break;
+
+    case 2:
+    ChangeRowsColumnsArray();
+    break;
+
+    case 3:
+    FrequencyDictionary();
+    break;
+
+    case 4:
+    RemoveMinElemArray();
+    break;
+
+    default:
+    Console.WriteLine("Номер задачи введен некорректно");
+    break;
+}
+
+int Prompt(string message)     // Метод запроса
+{
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+
+// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+void ChangeRowsArray()
+{
+    int[,] array = TwoRandomArray();
+    System.Console.WriteLine();
+    System.Console.WriteLine("Измененный массив");
+    ReadArray(ChangedRowsArray(array));
+}
+
+int [,] TwoRandomArray()     // функция формирования массива заданных параметров
+{
+    Console.WriteLine("Введите количество строк двумерного массива: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите количество столбцов двумерного массива: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int[,] array = new int[m,n];
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-10, 11);
+            Console.Write(array[i, j] + " ");
+        }
+    }
+    return array;
+}
+
+int[,] ChangedRowsArray(int[,] array)  // функция замены строк массива
+{
+    int m = array.GetLength(0);
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        int tmp = array[0,j];
+        array[0, j] = array[m - 1, j];
+        array[m - 1, j] = tmp;
+    }
+    return array;
+}
+
+void ReadArray(int[,] array)            // функция вывода двумерного массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно, 
+// программа должна вывести сообщение для пользователя.
+
+void ChangeRowsColumnsArray()
+{
+    int[,] array = TwoRandomArray();
+    System.Console.WriteLine();
+    System.Console.WriteLine();
+    // if (array.GetLength(0) != array.GetLength(1))
+    //     Console.WriteLine("Замена строк на столбцы невозможна в связи с неравенством количества элементов. Задайте квадратный массив");
+    // else
+        Console.WriteLine("Измененный массив");
+        ReadArray(ChangeRowsColumns(array));
+        System.Console.WriteLine();
+}
+
+int [,] ChangeRowsColumns(int[,] array)
+{
+    int[,] arrayChange = new int[array.GetLength(1),array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            arrayChange[j, i] = array[i, j];
+        }    
+    }
+    return arrayChange;
+}
+
+// Задача 57: Составить частотный словарь элементов двумерного массива. 
+// Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+
+void FrequencyDictionary()
+{
+    int[,] array = TwoRandomArray();
+    System.Console.WriteLine("\n");
+    int [] convertedArray = ConvertArray(array);   
+    // if (array.GetLength(0) != array.GetLength(1))
+    //     Console.WriteLine("Замена строк на столбцы невозможна в связи с неравенством количества элементов. Задайте квадратный массив");
+    // else
+    Array.Sort(convertedArray);
+    Console.WriteLine(String.Join(" ", convertedArray));
+    FrequencyDict(convertedArray);
+}
+
+int [] ConvertArray (int [,] array)
+{
+    int m = array.GetLength(0);
+    int n = array.GetLength(1);
+    int arrayIndex = 0;
+    int[] newArray = new int [m*n];
+    foreach (int item in array)
+    {
+        newArray[arrayIndex] = item;
+        arrayIndex ++;
+    }
+    return newArray;
+}
+
+void FrequencyDict(int[] array)
+{
+    int element = array[0];
+    int result = 1;
+    for (int i = 1; i <= array.Length+1; i++)
+    {
+        if (element == array[i])
+            result ++;
+        else
+        {
+            Console.WriteLine($"Элемент: {element} содержится в массиве {result} раз");
+            element = array[i];
+            result = 1;
+        }
+    }
+    Console.WriteLine($"Элемент: {element} содержится в массиве {result} раз");
+}
+
+// Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец, 
+// на пересечении которых расположен наименьший элемент массива.
+
+void RemoveMinElemArray()
+{
+    int [,] arr = TwoRandomArray();
+    Console.WriteLine("\n");
+    int [] indexMinValue =  FindMinEl(arr);
+    Console.WriteLine("Измененный массив");
+    DeleteRowColumn(arr, indexMinValue);
+    Console.WriteLine("\n");
+}
+
+int[] FindMinEl(int[,] arr)
+{
+    int minValue = arr[0,0];
+    int[] indexMinValue = new int [2];
+   for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if(minValue > arr[i,j])
+            {
+                minValue = arr[i,j];
+                indexMinValue[0] = i;
+                indexMinValue[1] = j;
+            }
+        }
+    }
+    Console.WriteLine($"Минимальный элемент массива: {minValue}");
+    return indexMinValue;
+}
+void DeleteRowColumn(int[,] arr, int[] index)
+{
+    int [,] newArr = new int[arr.GetLength(0) - 1, arr.GetLength(1) - 1];
+    int a = 0, b = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        if (index[0] == i) continue;
+        for (int j = 0; j < arr.GetLength(1); j++)
+        { 
+            if (index[1] == j) continue;
+
+            newArr[a,b] = arr[i,j];
+            b++;
+        }
+        a ++;
+        b = 0;
+    } 
+    ReadArray(newArr);    
+}
---
+++ home_work_s_8/Program.cs
@@ -0,0 +1,175 @@
+﻿Console.Clear();
+Console.WriteLine("1 - Программа для упорядочивания по убыванию элементов двумерного массива (задача 54)");
+Console.WriteLine("2 - Программа для нахождения строки с наименьшей суммой элементов в заданном прямоугольном двумерном массиве (задача 56)");
+Console.WriteLine("3 - Программа для нахождения произведения двух матриц (задача 58)");
+
+int numberOfTask = Prompt("Введите номер задачи ");
+switch (numberOfTask)
+{
+    case 1:
+    DecreasElementsRow();
+    break;
+
+    case 2:
+    RowMinSumElemArray();
+    break;
+
+    case 3:
+    ProductMatrices();
+    break;
+
+    default:
+    Console.WriteLine("Номер задачи введен некорректно");
+    break;
+}
+
+int Prompt(string message)     // Метод запроса
+{
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:              В итоге получается вот такой массив:
+// 1 4 7 2                              7 4 2 1
+// 5 9 2 3                              9 5 3 2
+// 8 4 2 4                              8 4 4 2
+
+void DecreasElementsRow()          // итоговая функция задачи 54
+{
+    int[,] array = TwoRandomArray();
+    System.Console.WriteLine();
+    System.Console.WriteLine("Измененный массив");
+
+    ReadArray(DecreasElements(array));
+
+}
+
+int [,] DecreasElements(int[,] array)       // функция упорядочивания по убыванию строк массива
+{
+    int[,] arrChange = new int[array.GetLength(0),array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int minPosition = j;
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i,k] > array[i,minPosition])
+                    minPosition = k;
+            } 
+            int temp = array[i,j];  // временный элемент
+            arrChange[i,j] = array[i,minPosition];
+            array[i,minPosition] = temp;     
+        }
+    } return  arrChange; 
+}
+
+int [,] TwoRandomArray()     // функция формирования массива заданных параметров
+{
+    Console.WriteLine("Введите количество строк двумерного массива: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите количество столбцов двумерного массива: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int[,] array = new int[m,n];
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-5, 20);
+            Console.Write(array[i, j] + " ");
+        }
+    }
+    return array;
+}
+
+void ReadArray(int[,] array)            // функция вывода двумерного массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+void RowMinSumElemArray()         // итоговая функция задачи 56
+{
+    int[,] array = TwoRandomArray();
+    System.Console.WriteLine("\n");
+    RowMinSumElem(array);
+    System.Console.WriteLine("\n");
+}
+
+void RowMinSumElem(int[,] array)     // функция, которая находит номер строки с наименьшей суммой элементов
+{
+    int sum = 0, minPositionRow = 0;
+    int minim = 20*array.GetLength(1);
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum = 0;
+        minPositionRow = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum = sum + array[i,j];
+        }
+        Console.WriteLine($"Сумма элементов строки {i+1} =  {sum}.");
+        if (sum < minim) 
+        {
+            minim = sum;
+            minPositionRow = i;
+        }
+    } 
+    Console.WriteLine($"Номер строки массива c наименьшей суммой элементов -  {minPositionRow+1} (индекс строки: {minPositionRow}).");
+}
+
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:                Результирующая матрица будет:
+// 2 4 | 3 4                                18 20
+// 3 2 | 3 3                                15 18
+
+void ProductMatrices()       // итоговая функция задачи 58
+{
+    System.Console.WriteLine("Матрица А");
+    int[,] arrayOne = TwoRandomArray();
+    System.Console.WriteLine("\n");
+    System.Console.WriteLine("Матрица В");
+    int[,] arraySec = TwoRandomArray();
+    System.Console.WriteLine("\n");
+    System.Console.WriteLine("Матрица произведения А*В");
+    ReadArray(ProductMatrice(arrayOne, arraySec));
+}
+
+int [,] ProductMatrice(int[,] arrayOne, int[,] arraySec)
+{
+    int [,] arrProduct = new int[arrayOne.GetLength(0), arraySec.GetLength(1)];
+    if (arrayOne.GetLength(1) != arraySec.GetLength(0))
+        Console.WriteLine("Произведение матриц найти невозможно, так как количество столбцов матрицы А не равно количеству строк матрицы В.");
+    else
+    {
+        for (int i = 0; i < arrayOne.GetLength(0); i++)
+        {
+            for (int j = 0; j < arraySec.GetLength(1); j++)
+            {  
+                for (int k = 0; k < arraySec.GetLength(0); k++)
+                {
+                    arrProduct[i,j] += arrayOne[i,k] * arraySec[k,j];
+                }
+            }
+        }
+    } return arrProduct;
+}
