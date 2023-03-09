04875f0128f49e1a9a0dadc1c9321f2a3ee82fd4
Anna Lutsevich <alucevich@mail.ru>
2023-03-09 14:06:05 +0700

    Add leccons 60,62 in homework

--- home_work_s_8/Program.cs
+++ home_work_s_8/Program.cs
@@ -2,6 +2,9 @@
 Console.WriteLine("1 - Программа для упорядочивания по убыванию элементов двумерного массива (задача 54)");
 Console.WriteLine("2 - Программа для нахождения строки с наименьшей суммой элементов в заданном прямоугольном двумерном массиве (задача 56)");
 Console.WriteLine("3 - Программа для нахождения произведения двух матриц (задача 58)");
+Console.WriteLine("4 - Программа для вывода с индексами трехмерного массива из неповторяющихся двухзначных чисел (задача 60)");
+Console.WriteLine("5 - Программа для спирального заполнения массива 4 на 4 (задача 62)");
+
 
 int numberOfTask = Prompt("Введите номер задачи ");
 switch (numberOfTask)
@@ -16,6 +19,15 @@ switch (numberOfTask)
 
     case 3:
     ProductMatrices();
+    break;
+
+    case 4:
+    ThreeArray();
+    break;
+
+    case 5:
+    //FourRandomArray(4,4);
+    ReadArray(FourSpiralArray(4,4));
     break;
 
     default:
@@ -173,3 +185,174 @@ int [,] ProductMatrice(int[,] arrayOne, int[,] arraySec)
         }
     } return arrProduct;
 }
+
+// Задача 60: Сформируйте трехмерный массив из неповторяющихся двухзначных чисел. Напишите программу, которая будет построчно выводить
+// массив, добавляя индексы каждого элемента.
+// Например, массив размером 2 х 2 х 2:
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+void ThreeArray()
+{
+    int[,,] threeArray = ThreeRandomArray();
+    ReadThreeArrayIndex(threeArray);
+    Console.WriteLine();
+}
+
+int [,,] ThreeRandomArray()
+{
+    Console.WriteLine("Введите размерность трехмерного массива: ");
+    Console.Write("m =   ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.Write("n =   ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.Write("k =   ");
+    int l = Convert.ToInt32(Console.ReadLine()); 
+    
+    int[,,] array = new int[m,n,l];
+    int N = m*n*l;
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < l; k++)
+            {
+                array[i, j, k] = GenerateUniqueNumber();
+            }
+        }
+    } return array;
+}
+
+int GenerateUniqueNumber()
+{
+    HashSet<int> numbers = new HashSet<int>();
+    // int [] numbers = new int [N*2];
+    Random r = new Random();
+    while (true)
+    {
+        var n = r.Next(10, 100);
+        if (!numbers.Contains(n))
+        {
+            numbers.Add(n);
+            return n;
+        }
+    }
+}
+       
+void ReadThreeArrayIndex(int[,,] array)            // функция вывода трехмерного массива с индексами
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write(array[i, j, k] + $"({i},{j},{k})  ");
+            }
+        System.Console.WriteLine();
+        }
+    }
+}
+
+
+// Задача 62: Напишите программу, которая заполнит спирально массив 4 на 4
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+int [,] FourSpiralArray(int m,int n)     // функция формирования спирального массива 
+{
+    int[,] spiralArrayarray = new int[n, m];
+ 
+    int row = 0;
+    int col = 0;
+    int dx = 1;
+    int dy = 0;
+    int dirChanges = 0;
+    int visits = m;
+
+    for (int i = 0; i < spiralArrayarray.Length; i++) 
+    {
+        spiralArrayarray[row, col] = i + 1;
+        if (--visits == 0) 
+        {
+            visits = m * (dirChanges %2) + n * ((dirChanges + 1) %2) - (dirChanges/2 - 1) - 2;
+            int temp = dx;
+            dx = -dy;
+            dy = temp;
+            dirChanges++;
+        }
+        col += dx;
+        row += dy;
+    } return spiralArrayarray;
+}
+
+
+// Задача 61: Вывести первые N строк треугольника Паскаля. 
+// Сделать вывод в виде равнобедренного треугольника.
+
+
+
+
+
+
+
+
+
+
+// int [,,] ThreeRandomArray()     // функция формирования трехмерного двузначного массива заданных параметров
+// {
+//     Console.WriteLine("Введите размерность трехмерного массива: ");
+//     Console.Write("m =   ");
+//     int m = Convert.ToInt32(Console.ReadLine());
+//     Console.Write("n =   ");
+//     int n = Convert.ToInt32(Console.ReadLine());
+//     Console.Write("k =   ");
+//     int l = Convert.ToInt32(Console.ReadLine()); 
+    
+//     int[,,] array = new int[m,n,l];
+    
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         Console.WriteLine();
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.WriteLine();
+//             for (int k = 0; k < array.GetLength(2); k++)
+//             {
+//                 array[i, j, k] = new Random().Next(10, 100);
+//                 Console.Write(array[i, j, k] + " ");
+//             }
+//         }
+//     } return array;
+// }
+// int [,] FourRandomArray(int a, int b)     // функция формирования четырехмерного массива заданных параметров
+// {
+//     int[,] array = new int[a,b];
+//     int e = 0;
+    
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         Console.WriteLine();
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             e += 1;
+//             array[i, j] = e;
+//             Console.Write(array[i, j] + " ");
+//         }
+//     } return array;
+// }
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     Console.WriteLine();
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         e += 1;
+//         array[i, j] = e;
+//         Console.Write(array[i, j] + " ");
+//     }
