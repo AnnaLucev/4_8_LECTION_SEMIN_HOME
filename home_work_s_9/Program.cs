f867ccf4f05209bf1d1e11e86dc24f7d4ae94c82
Anna Lutsevich <alucevich@mail.ru>
2023-03-10 19:24:12 +0700

    Add homework 64

--- home_work_s_9/Program.cs
+++ home_work_s_9/Program.cs
@@ -1,15 +1,20 @@
 ﻿Console.Clear();
-Console.WriteLine("1 - Программа для вывода суммы всех натуральных между M и N через рекурсию (задача 66)");
-Console.WriteLine("2 - Программа для вычисления функции Аккермана с помощью рекурсии (задача 68)");
+Console.WriteLine("1 - Программа для вывода натуральных чисел в промежутке от N до 1 с помощью рекурсии (задача 64)");
+Console.WriteLine("3 - Программа для вывода суммы всех натуральных между M и N через рекурсию (задача 66)");
+Console.WriteLine("3 - Программа для вычисления функции Аккермана с помощью рекурсии (задача 68)");
 
 int numberOfTask = Prompt("Введите номер задачи ");
 switch (numberOfTask)
 {
     case 1:
-    SumNumbersRec();
+    NumbersIntervalRec();
     break;
 
     case 2:
+    SumNumbersRec();
+    break;
+
+    case 3:
     AkkermanRec();
     break;
 
@@ -25,6 +30,25 @@ int Prompt(string message)     // Метод запроса
     return result;
 }
 
+// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void NumbersIntervalRec()
+{
+    Console.WriteLine("Введите число больше 0: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(NumbersInterval(m));
+}
+
+int NumbersInterval(int m)
+{
+    if (m==1) return 1;
+    Console.Write($"{m}, ");
+    return NumbersInterval(m-1);
+}
+
+
 // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
@@ -32,7 +56,7 @@ int Prompt(string message)     // Метод запроса
 void SumNumbersRec() 
 {
     Console.WriteLine("Введите минимальное число: ");
-    int m = Convert.ToInt32(Console.ReadLine());
+    int m = Convert.ToInt32cd(Console.ReadLine());
     Console.WriteLine("Введите максимальное число: ");
     int n = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine(SumNumbers(m, n));
